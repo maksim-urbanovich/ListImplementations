4fca4b62645b9a1add15639628b0687ee4e944b1
maksim-urbanovich <MaximusUrb@gmail.com>
2021-08-23 08:32:46 +0300

    Initial commit

---
+++ src/main/java/com/maximus/jwd/FlexibleArray.java
@@ -0,0 +1,11 @@
+package com.maximus.jwd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlexibleArray {
+    private final static int INITIAL_CAPACITY = 16;
+    List<Integer> list = new ArrayList<>();
+
+    private int capacity;
+}
---
+++ src/main/java/com/maximus/jwd/Main.java
@@ -0,0 +1,8 @@
+package com.maximus.jwd;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
