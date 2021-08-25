635744a68e225f260cbf017abadf5bc2d480b564
maksim-urbanovich <MaximusUrb@gmail.com>
2021-08-25 23:43:29 +0300

    Added constructors

--- src/main/java/com/maximus/jwd/FlexibleArray.java
+++ src/main/java/com/maximus/jwd/FlexibleArray.java
@@ -1,11 +1,39 @@
 package com.maximus.jwd;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
-public class FlexibleArray {
-    private final static int INITIAL_CAPACITY = 16;
-    List<Integer> list = new ArrayList<>();
+public class FlexibleArray<E> implements Cloneable{
+    private final static int INITIAL_CAPACITY = 10;
 
-    private int capacity;
+    private final static Object[] EMPTY_ELEMENTDATA = {};
+    private final static Object[] DEFAULT_CAPACITY_EMPTY_ELEMENTDATA = {};
+
+    transient Object[] elementData;
+
+    private int size;
+
+    public FlexibleArray() {
+        this.elementData = DEFAULT_CAPACITY_EMPTY_ELEMENTDATA;
+    }
+
+    public FlexibleArray(int capacity) {
+        if (capacity > 0) {
+            this.elementData = new Object[capacity];
+        } else if (capacity == 0){
+            this.elementData = EMPTY_ELEMENTDATA;
+        }
+        else {
+            throw new IllegalArgumentException("Wrong list capacity" + capacity);
+        }
+    }
+
+    public FlexibleArray(Collection<? extends E> collection) {
+        this.elementData = collection.toArray();
+        if (elementData.length == 0) {
+
+        }
+    }
 }
