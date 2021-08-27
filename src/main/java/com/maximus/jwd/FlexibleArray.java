9e8a3f404252f2333e4f29c412dd2a4a01a6bdc3
maksim-urbanovich <MaximusUrb@gmail.com>
2021-08-27 23:57:52 +0300

    Added methods

--- src/main/java/com/maximus/jwd/FlexibleArray.java
+++ src/main/java/com/maximus/jwd/FlexibleArray.java
@@ -1,9 +1,6 @@
 package com.maximus.jwd;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class FlexibleArray<E> implements Cloneable{
     private final static int INITIAL_CAPACITY = 10;
@@ -34,6 +31,72 @@ public class FlexibleArray<E> implements Cloneable{
         this.elementData = collection.toArray();
         if (elementData.length == 0) {
             elementData = EMPTY_ELEMENTDATA;
+        } else {
+            elementData = Arrays.copyOf(elementData, elementData.length, Object[].class);
+        }
+    }
+
+    private void add (E element, Object[] elementData, int s) {
+        if (s == elementData.length) {
+            elementData = grow();
+        } else {
+            elementData[s] = element;
+            size++;
+        }
+    }
+
+    private Object[] grow() {
+
+    }
+
+    public void add(E element) {
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public int indexOf(Object object) {
+        if (object == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if(object.equals(elementData[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains (Object object) {
+        return (indexOf(object) != -1);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index > size) {
+            throw new RuntimeException("Index out of bound");
+        } else {
+            return (E) elementData[index];
+        }
+    }
+
+    public E set(int index, E newValue) {
+        if (index < 0 || index > size) {
+            throw new RuntimeException("Index out of bound");
+        } else {
+            E oldValue = (E) elementData[index];
+            elementData[index] = newValue;
+            return oldValue;
         }
     }
 }
